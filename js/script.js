64d111d0cc666ccbc60c426e0b83c2670abaf50c
Soham <sohamchavan368@gamil.com>
2026-07-17 23:35:07 +0530

    Initial NovaAI Project

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// JavaScript Connected
+console.log("NovaAI Loaded Successfully!");
+
+// Welcome Popup
+alert("Welcome to NovaAI!");
+
+// Select Hero Button
+const button = document.getElementById("start-btn");
+
+// Print Button in Console
+console.log(button);
+
+// Click Event
+button.addEventListener("click", function(){
+
+    alert("Thanks for choosing NovaAI!");
+
+});
